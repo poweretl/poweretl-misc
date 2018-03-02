1163d46c984dd5d02fd8e64eddca830dff594521
Joshua <jnwankwo@acm.org>
2018-03-02 19:24:10 +0800

    Inital import

---
+++ srcfiles/Refactored2.java
@@ -0,0 +1,11 @@
+public class Refactored2 {
+  
+  public static String renderPageWithSetupsAndTeardowns(
+      PageData pageData, boolean isSuite) throws Exception {
+      if (isTestPage(pageData))
+      includeSetupAndTeardownPages(pageData, isSuite);
+      return pageData.getHtml();
+      }
+  
+  
+}
